c5ee0015c0cbcee0da85057ae0a7a302250e27ea
AbonyiXavier <francisabonyi@gmail.com>
2020-01-08 09:09:01 -0800

    adjusted the order of the console.log

--- compound-Interest.js
+++ compound-Interest.js
@@ -11,7 +11,7 @@
 // declare time in some variable say time with a value.
 // declare rate in some variable say rate with a value.
 // Calculate compound interest using formula,
-// CompoundInterest = principle * (1 + rate / 100)  time) or principle * Math.pow(1 + rate / (n * 100), n * time) .
+// CompoundInterest = principal * (1 + rate / 100)  time) or principal * Math.pow(1 + rate / (n * 100), n * time) .
 // Finally, print the resultant value of CompoundInterest.
 
 // declaring of variables
@@ -20,11 +20,11 @@ let rate = 0.05;
 let n = 12; // n = the number of times that interest is compounded per unit
 let time = 10;
 
-// let CompoundInterest = principle * Math.pow(1 + rate / 100, time);
+// let CompoundInterest = principal * Math.pow(1 + rate / 100, time);
 let CompoundInterest = principal * Math.pow(1 + rate / (n * 100), n * time);
+
+console.log(CompoundInterest);
 console.log("The total amount is " + CompoundInterest.toFixed(2));
 console.log(
   "The interest is " + (CompoundInterest.toFixed(2) - principal).toFixed(2)
 );
-
-console.log(CompoundInterest);
